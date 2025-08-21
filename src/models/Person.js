040bb907d7a52af59fef5d1d29d1db84802ed3ab
Cynthia Loh <cynthia.loh1011@gmail.com>
2025-08-20 18:39:20 -0700

    person class

---
+++ src/models/Person.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
